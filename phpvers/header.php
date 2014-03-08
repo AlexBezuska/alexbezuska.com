dd040a65a39d47cb9669d7b5d39d442ce1cef8d6
Alex Bezuska <abezuska@gmail.com>
2014-03-08 09:13:10 -0500

    fix responsive | spring cleaning | add resume files

--- phpvers/header.php
+++
@@ -1,9 +0,0 @@
-
-  <link rel="stylesheet" href="css/main.css"/>
-</head>
-
-<body>
-
-<div class="logo normal"><a href="index.html"><img src="img/glasses.png"></a></div>
-
-<?php inlcude('nav.php'); ?>
