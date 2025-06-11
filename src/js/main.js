809a031cf236e7235c20f00578030713a43b1c52
fufroom <hello.fufroom@gmail.com>
2025-06-11 18:19:00 -0400

    big 2025 update

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,8 @@ ga('create', '{{google-analytics-id}}', 'auto'); ga('send', 'pageview')
 
 const currentYear = new Date().getFullYear();
 
-const yearsPhotoshop = currentYear - 1995;
-const yearsGit = currentYear - 2013;
+const yearsPhotoshop = currentYear - 1996;
+const yearsGit = currentYear - 2014;
 const yearsFrontend = currentYear - 2009;
 
 
@@ -17,27 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-$(function(){
-  $('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
-  });
-  
-  $(window).scroll(function() {
-    if(scrolled('below', '.bg-image-alex')){
-      $('body').removeClass('bg-blue-sm');
-      $('.navbar').addClass('bg-blue');
-      $('.navbar').removeClass('bg-grey');
-      $('.jumbotron').addClass('darken-jumbotron');
-    }
-    
-    if(scrolled('above', '.bg-image-alex')){
-      $('body').addClass('bg-blue-sm');
-      $('.navbar').removeClass('bg-blue');
-      $('.navbar').addClass('bg-grey');
-      $('.jumbotron').removeClass('darken-jumbotron');
-    }
-  });
-});
+
 
 function scrolled(direction, selector){
   var scroll = $(window).scrollTop();
@@ -45,3 +25,39 @@ function scrolled(direction, selector){
   var ht = $(selector).height();
   return direction === "below" ? scroll > os + ht : scroll < os + ht;
 }
+
+
+
+const card = document.querySelector('.float-card');
+const container = document.querySelector('.card-container');
+
+document.addEventListener('mousemove', (e) => {
+  const { innerWidth: w, innerHeight: h } = window;
+  const x = (e.clientX / w - 0.5) * 2; // from -1 to 1
+  const y = (e.clientY / h - 0.5) * 2;
+
+  const rotateX = y * 10; // max tilt angle
+  const rotateY = -x * 10;
+
+  card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+});
+
+
+function shuffleStack(stack) {
+  const images = Array.from(stack.querySelectorAll('.polaroid'));
+  if (images.length <= 1) return;
+
+  // Move the top image to the back of the stack
+  const top = images.pop();
+  stack.removeChild(top);
+  stack.insertBefore(top, images[0]);
+
+  // Reassign classes to update z-index + rotation
+  const updated = Array.from(stack.querySelectorAll('.polaroid'));
+  updated.forEach((img, i) => {
+    img.style.setProperty('--angle', `${[-6, 3, -2, 5, -4][i % 5]}deg`);
+    img.style.setProperty('--depth', `${i * 10}px`);
+    img.style.zIndex = i + 1;
+  });
+}
+
--- static-site-generator.js
+++ static-site-generator.js
@@ -1,36 +1,77 @@
-var fs = require("fs");
-var path = require("path");
-var handlebars = require("handlebars");
-var copydir = require("copy-dir");
+const fs = require("fs");
+const path = require("path");
+const handlebars = require("handlebars");
+const copydir = require("copy-dir");
 
-var site = require("./site.json");
+const src = "./src";
+const dest = "./dest";
+const dataDir = path.join(__dirname, "data");
+const partialsDir = path.join(src, "partials");
 
-var src = "./src";
-var dest = "./dest";
-
-// Register helpers BEFORE compiling templates
-handlebars.registerHelper("isEven", function (value) {
-  return value % 2 === 0;
-});
-
-handlebars.registerHelper("dashify", function (str) {
+// Register Handlebars helpers
+handlebars.registerHelper("isEven", (value) => value % 2 === 0);
+handlebars.registerHelper("dashify", (str) => {
   if (typeof str !== "string") return "";
   return str.toLowerCase().replace(/\s+/g, '-');
 });
+handlebars.registerHelper("json", function (context) {
+  return JSON.stringify(context, null, 2);
+});
 
+handlebars.registerHelper('even', function(index) {
+  return index % 2 === 0;
+});
+
+
+// Initialize site data object
+const site = {};
+
+// Load all JSON files in /data
+fs.readdirSync(dataDir).forEach((file) => {
+  if (file.endsWith(".json")) {
+    const key = path.basename(file, ".json").replace(/-([a-z])/g, (_, c) => c.toUpperCase());
+    const data = require(path.join(dataDir, file));
+
+    // Special-case for site.json: merge directly into root
+    if (key === "site") {
+      Object.assign(site, data);
+    } else {
+      site[key] = data;
+    }
+  }
+});
+
+// DEBUG: Output the site object to console
+console.log("✅ Loaded site data:");
+console.log(JSON.stringify(site, null, 2));
+
+// Register Handlebars partials from /src/partials
+if (fs.existsSync(partialsDir)) {
+  fs.readdirSync(partialsDir).forEach((filename) => {
+    if (filename.endsWith(".hbs")) {
+      const name = path.basename(filename, ".hbs");
+      const content = fs.readFileSync(path.join(partialsDir, filename), "utf8");
+      handlebars.registerPartial(name, content);
+    }
+  });
+}
+
+// Ensure destination directory exists
 makeDirIfNotExist(dest);
 
-console.log(site);
-var template = fs.readFileSync(path.join(src, "index.hbs"), "utf8");
-var compiledTemplate = handlebars.compile(template);
-var html = compiledTemplate(site);
+// Compile and write HTML
+const template = fs.readFileSync(path.join(src, "index.hbs"), "utf8");
+const compiledTemplate = handlebars.compile(template);
+const html = compiledTemplate(site);
 fs.writeFileSync(path.join(dest, "index.html"), html);
 
+// Copy static assets
 copyDirectory(path.join(src, "css"), path.join(dest, "css"));
 copyDirectory(path.join(src, "img"), path.join(dest, "img"));
 copyDirectory(path.join(src, "icons"), path.join(dest, "icons"));
 copyDirectory(path.join(src, "js"), path.join(dest, "js"));
 
+// Helpers
 function makeDirIfNotExist(filePath) {
   if (!fs.existsSync(filePath)) {
     fs.mkdirSync(filePath, { recursive: true });
